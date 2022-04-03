b52f83a0af4cb8373427ee699f443bbc706ba6d6
Пілецький Володимир <piletskiy10@gmail.com>
2022-04-03 16:23:27 +0300

    more recursive functions added

--- recursive_functions.py
+++ recursive_functions.py
@@ -8,14 +8,24 @@ def recursive_sum_of_arr_elements(arr: List[int], n: int) -> int:
     return recursive_sum_of_arr_elements(arr, n - 1) + arr[n - 1]
 
 
-def sum_of_arr_elements(arr: List[int], n: int) -> int:
-    result = 0
+def recursive_number_range(start: int, end: int) -> List[int]:
+    if end == start:
+        return [start]
 
-    for el in arr:
-        result += el
-
+    result = recursive_number_range(start, end - 1)
+    result.append(end)
     return result
 
 
+def recursive_countdown(n: int) -> List[int]:
+    if n < 1:
+        return []
+
+    count_array = recursive_countdown(n - 1)
+    count_array.insert(0, n)
+    return count_array
+
+
 print(recursive_sum_of_arr_elements([1, 2, 3, 4], 4))
-print(sum_of_arr_elements([1, 2, 3, 4], 4))
+print(recursive_number_range(1, 5))
+print(recursive_countdown(5))
